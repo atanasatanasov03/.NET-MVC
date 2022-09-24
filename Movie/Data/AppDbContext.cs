a05e550685a6783940309cf001b179f09e442677
Atanas Atanasov <atanas.p.atanasov.2017@elsys-bg.org>
2022-09-24 12:04:38 +0300

    Add project files.

---
+++ Movie/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movie.Models;
+using System.Reflection;
+
+namespace Movie.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) {}
+
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
